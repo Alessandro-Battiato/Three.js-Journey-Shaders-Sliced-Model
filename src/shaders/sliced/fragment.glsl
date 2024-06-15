12800ff810078e631da5105af461031f20f18293
alessandrobattiato <alessandroDev@outlook.it>
2024-06-15 10:15:43 +0200

    feat: sliced portion implemented

--- src/shaders/sliced/fragment.glsl
+++ src/shaders/sliced/fragment.glsl
@@ -1,3 +1,15 @@
+varying vec3 vPosition;
+
 void main() {
-    csm_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    float uSliceStart = 1.0;
+    float uSliceArc = 1.5;
+
+    float angle = atan(vPosition.y, vPosition.x); // MANDATORY to put FIRST the y axis and THEN the x axis
+
+    if (angle > uSliceStart && angle < uSliceStart + uSliceArc) {
+        // The sliced portion occurs only when the angle satisfies the said conditions, and if it does, we discard all of the pixels so that they are not drawn
+        discard;
+    }
+
+    csm_FragColor = vec4(vec3(angle), 1.0);
 }
