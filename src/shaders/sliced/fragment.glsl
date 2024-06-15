fe0b194a14a56ba54e47fc585d6197fe3203a229
alessandrobattiato <alessandroDev@outlook.it>
2024-06-15 11:06:39 +0200

    fix: fixed slice model issue

--- src/shaders/sliced/fragment.glsl
+++ src/shaders/sliced/fragment.glsl
@@ -8,8 +8,10 @@ void main() {
     // float uSliceArc = 1.5;
 
     float angle = atan(vPosition.y, vPosition.x); // MANDATORY to put FIRST the y axis and THEN the x axis
+    angle -= uSliceStart;
+    angle = mod(angle, PI2);
 
-    if (angle > uSliceStart && angle < uSliceStart + uSliceArc) {
+    if (angle > 0.0 && angle < uSliceArc) {
         // The sliced portion occurs only when the angle satisfies the said conditions, and if it does, we discard all of the pixels so that they are not drawn
         discard;
     }
